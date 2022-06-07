0f89972399e1189480d12ac5243ed3b3461103f9
Allen Poston <73608888+allenposton@users.noreply.github.com>
2022-06-06 23:13:39 -0400

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,26 @@
+alert("chat.js is activated");
+
+
+
+
+function test(){
+  alert("Yep");
+  
+}
+
+
+
+
+/// User Input
+  
+function handle(e){
+    var key = e.which;
+    if (key == 13){
+      alert("_____HELLO  YOUR INPUT WORKS BUT THIS CODE IS A WORKING PROGRESS SORRY =[ ___________");
+      getInput();
+    }
+  }
+  
+  function getInput(){
+    document.body.style.backgroundColor  = user-input.value;
+  }
