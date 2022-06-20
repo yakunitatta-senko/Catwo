54d9220c42bf1f81dfd70ce84e793eeae1bb27b7
Allen Poston <73608888+allenposton@users.noreply.github.com>
2022-06-20 16:18:47 -0400

    Update chat.js

--- static/JS/chat.js
+++ static/JS/chat.js
@@ -1,30 +1,40 @@
-var input = document.getElementById("user-input");
 
+var input = getInput(input);
+document.getElementById("user").innerHTML = input.value;
 
-
-
-function test(){
-  alert("Yep");
-  
+function getInput(input) {
+  return input;
 }
 
 
+/// Shows the users text while typeing
+function ShowUserText() {
+  var input = document.getElementById("user-input");
+  
+  const ShowUserText = document.getElementById("User").innerHTML = input.value;
+  
+}
+
+function test() {
+  alert("Yep");
+
+}
 
 
 /// User Input
-  
-function handle(e){
-    var key = e.which;
-    if (key == 13){
-      alert(input.value);
-      getInput();
-    }
+
+function handle(e) {
+  var key = e.which;
+  if (key == 13) {
+    alert(input.value);
+    getInput();
   }
-  
+}
+
 function getInput() {
- var input = document.getElementById("user-input");
-  
-  document.body.style.backgroundColor= input.value;
-  
+  var input = document.getElementById("user-input");
+
+  document.html.style.backgroundColor = input.value;
+
   document.getElementById("User").innerHTML = input.value;
 }
