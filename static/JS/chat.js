4613af2b0b0dfa21c43d07cef67babe362823cf2
Allen Poston <73608888+allenposton@users.noreply.github.com>
2022-07-12 13:03:30 -0400

    Update chat.js
    Simplified the input value using JQuery becuase it is BTA (#1 ) BEtter

--- static/JS/chat.js
+++ static/JS/chat.js
@@ -1,39 +1,19 @@
-var input = getInput(input);
-document.getElementById("user").innerHTML = input.value;
-var input = document.getElementById("user-input");
 
+//Gets the text of the user
 
-function getInput(input) {
-  return input;
-}
+$("#user-input").keyup(function() {
+  $("#User").html($(this).val());
+});
 
-
-/// Shows the users text while typeing
-function ShowUserText() {
-  var input = document.getElementById("user-input");
-  const ShowUserText = document.getElementById("User").innerHTML = input.value;
-  document.getElementById("User").innerHTML = input.value;
-
-}
-
-/// press enter function
 function handle(e) {
   var input = document.getElementById("user-input");
   var key = e.which;
   if (key == 13) {
-    alert(input.value);
+    alert('*Clearing*: ' +input.value);
     getInput()
   }
 
-  //show what user is clicking
-  function test() {
-    alert(document.addEventListener);
-
-  }
-}
-
-/// User Input
 function getInput() {
   var input = document.getElementById("user-input");
   document.getElementById("User").innerHTML = input.value = "";
-}
+}}
