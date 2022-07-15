4f988b43edffe423fc445300931e8f01e826c2e3
Allen Poston <73608888+allenposton@users.noreply.github.com>
2022-07-14 21:59:29 -0400

    Update chat.js
    Added text to speech feature

--- static/JS/chat.js
+++ static/JS/chat.js
@@ -1,4 +1,3 @@
-
 //Gets the text of the user
 
 $("#user-input").keyup(function() {
@@ -9,11 +8,40 @@ function handle(e) {
   var input = document.getElementById("user-input");
   var key = e.which;
   if (key == 13) {
-    alert('*Clearing*: ' +input.value);
+    alert('*Clearing*: ' + input.value);
     getInput()
   }
 
-function getInput() {
-  var input = document.getElementById("user-input");
-  document.getElementById("User").innerHTML = input.value = "";
-}}
+  function getInput() {
+    var input = document.getElementById("user-input");
+    document.getElementById("User").innerHTML = input.value = "";
+  }
+}
+
+
+
+//This is the Text to speech input 
+{
+  const textInputField = document.querySelector("#user-input");
+  const form = document.querySelector("#user-input");
+  const utterThis = new SpeechSynthesisUtterance();
+  const synth = window.speechSynthesis;
+  let ourText = "";
+
+  const checkBrowserCompatibility = () => {
+    "speechSynthesis" in window
+      ?
+      console.log("Web Speech API supported!") :
+      console.log("Web Speech API not supported :-(")
+  }
+
+  checkBrowserCompatibility();
+//activates on double Clicked
+  form.ondblclick = (event) => {
+    event.preventDefault();
+    ourText = textInputField.value;
+    utterThis.text = ourText;
+    synth.speak(utterThis);
+    textInputField.value = document.getElementById('user-input').value;
+  }
+}
