e162ee87ec70518c7b5245cb4c23afdbe71186a6
Allen Poston <73608888+allenposton@users.noreply.github.com>
2022-07-31 02:04:00 -0400

    Update responses.js
    NOTHING REALLY UNESSASSARY CODE Bruh

--- static/JS/responses.js
+++ static/JS/responses.js
@@ -5,3 +5,43 @@ let greetings = [
   'Good day sir.',
   'Please go away!'
 ]
+
+let greetings = [
+  'Hello. How are you?',
+  'What\'s up dog?',
+  'What\'s up?',
+  'Good day sir.',
+  'Please go away!'
+]
+
+var input = document.getElementById('user-input');
+var human = input.value;
+
+
+var bot = document.getElementById('BotText');
+
+function test() {
+  document.getElementById("BotText").innerHTML = "hi";
+} {}
+
+
+import React, { useState } from 'react';
+
+const [location, setLocation] = useState("");
+
+return (
+
+);
+
+
+const adjustInputSize = () => {
+  const locationButton = document.getElementById("user-input")
+  if (location.length < 1) {
+    locationButton.style.width = locationButton.placeholder.length + 2 + 'ch'
+
+  } else {
+    locationButton.style.width = location.length + 5 + 'ch'
+
+  }
+}
+curl -X GET https://api.fakeyou.com/tts/list
