e3383bd01adc5ff801b5ea90d41bbd0483a00f54
Allen Poston <73608888+allenposton@users.noreply.github.com>
2022-07-31 02:02:37 -0400

    Update chat.js
    Made The Dbclick better

--- static/JS/chat.js
+++ static/JS/chat.js
@@ -5,25 +5,27 @@ $("#user-input").keyup(function() {
 });
 
 function handle(e) {
-  var input = document.getElementById("user-input");
+  let input = document.getElementById("user-input").value;
+  document.getElementById("User").innerHTML = input;
   var key = e.which;
   if (key == 13) {
-    alert('*Clearing*: ' + input.value);
-    getInput()
+    alert('*Clearing*: ' + input);
+    getInput(input);
   }
 
-  function getInput() {
-    var input = document.getElementById("user-input");
+  function getInput(input) {   
+    var input = document.getElementById("user-input");    
     document.getElementById("User").innerHTML = input.value = "";
+    let text = (input.toLowerCase()).replace(/[^\w\s\d]/gi, "");
   }
 }
 
 
 
-//This is the Text to speech input 
+//THis is the Text to speech input
 {
   const textInputField = document.querySelector("#user-input");
-  const form = document.querySelector("#user-input");
+  const form = document.querySelector("#User");
   const utterThis = new SpeechSynthesisUtterance();
   const synth = window.speechSynthesis;
   let ourText = "";
@@ -36,11 +38,10 @@ function handle(e) {
   }
 
   checkBrowserCompatibility();
-//activates on double Clicked
+
   form.ondblclick = (event) => {
     event.preventDefault();
     ourText = textInputField.value;
-    utterThis.text = ourText;
     synth.speak(utterThis);
     textInputField.value = "";
   }
